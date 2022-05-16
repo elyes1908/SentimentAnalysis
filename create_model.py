1f6ce7e8043091dfe0a50f89cb3da7013dc5a7b0
elyes1908 <elyes.tarmiz@esprit.tn>
2022-05-17 00:40:33 +0100

    first commit

---
+++ create_model.py
@@ -0,0 +1,11 @@
+import dill
+import tensorflow as tf
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine", use_fast=True)
+model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
+
+nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+dill.dump(nlp, open('predict.pkl', 'wb'))
