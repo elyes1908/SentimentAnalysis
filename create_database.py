3c80a5d815e3e422306af35ae1f933e1911578aa
elyes1908 <elyes.tarmiz@esprit.tn>
2022-05-17 01:13:08 +0100

    Fix project bugs

--- create_database.py
+++ create_database.py
@@ -1,3 +1,10 @@
 from app import db
 
 db.create_all()
+if __name__ == '__main__':
+    try:
+        db.create_all()
+        print('Database created.')
+    except Exception as e:
+        print('Database not created.')
+        print(e)
